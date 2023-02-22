4f10b2f4b17fd7e29c05d896c9005372f2c5864d
avinash singh <avinash8.as@gmail.com>
2023-02-21 20:46:18 -0500

    finish random task component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import Home from "./Home";
 import NavBar from "./Navbar";
+import { Route, Switch } from "react-router-dom";
+import RandomTask from "./RandomTask";
+import "../index.css"
 
 function App() {
   return (
     <>
       <NavBar />
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
+      <RandomTask />
     </>
-  );
+  )
 }
+  
+
 
 export default App;
---
+++ src/components/RandomTask.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+
+import Button from '@mui/material/Button';
+
+function RandomTask(){
+    const [randomActivity, setRandomActivity] = useState('')
+    const handleGenerateActivity = () => {
+        fetch('https://www.boredapi.com/api/activity')
+        .then(resp => resp.json())
+        .then(data => setRandomActivity(data.activity))
+    }
+    return (
+        <div className="header">
+            <h1 style={{textAlign: "center"}}>Can't Think of an Activity?</h1>
+            <h3>Click the button below to generate a random activity</h3>
+            <Button variant="contained" onClick={handleGenerateActivity}>GENERATE RANDOM ACTIVITY</Button>
+            <h1>{randomActivity}</h1>
+        </div>
+    )
+}
+
+export default RandomTask
