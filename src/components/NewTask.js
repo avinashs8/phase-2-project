a7b7fbb9c8c61f6aff9e666e9c6ef933de4ff0ba
avinash singh <avinash8.as@gmail.com>
2023-02-21 21:10:08 -0500

    adjust labels of forms

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -3,7 +3,32 @@ import TextField from '@mui/material/TextField';
 
 function NewTask() {
   return (
-    
+    <div className='header'>
+        <h4>Enter New Task</h4>
+        <Box
+        component="form"
+        sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        
+        >
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </Box>
+        <h4>Time</h4>
+        <Box
+        component="form"
+        sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        
+        >
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </Box>
+    </div>
   );
 }
 
