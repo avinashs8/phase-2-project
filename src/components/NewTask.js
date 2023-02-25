65e3e8676dd85f214864dc052ac5560f97d561d2
avinash singh <avinash8.as@gmail.com>
2023-02-24 20:52:05 -0500

    add css to form

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,39 +1,29 @@
-import Box from '@mui/material/Box';
+
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 function NewTask() {
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(e)
+    }
+
   return (
     <div className='header'>
-        <h4>Enter New Task</h4>
-        <Box
-        component="form"
-        sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-        
-        
-        >
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Box>
-        <h4>Time (in minutes)</h4>
-        <Box
-        component="form"
-        sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-        
-        >
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <br></br>
-        <Button variant="contained" type='submit'>Submit</Button>
-        </Box>
+        <form onSubmit={handleSubmit}>
+          <h4>Enter New Task</h4>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <h4>Time (in minutes)</h4>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <div className='button'></div>
+
+            <Button variant="contained" type='submit'>Submit</Button>
+        </form>
     </div>
   );
 }
 
 export default NewTask;
+
+{/*  */}
