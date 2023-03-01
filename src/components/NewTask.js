abbbcd22486a01be2c134c26ab26f8715f69558d
avinash singh <avinash8.as@gmail.com>
2023-02-28 20:26:55 -0500

    start controlled form

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,24 +1,37 @@
 
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import React, { useState } from 'react';
 
 function NewTask() {
-
-    function handleSubmit(e){
+  const [activity, setActivity] = useState('')
+  const [time, setTime ] = useState('')
+  
+  function handleSubmit(e){
         e.preventDefault()
-        console.log(e)
+        console.log(e.target.value)
     }
+
+  function handleNewActivity(e){
+    setActivity(e.target.value)
+    console.log(activity)
+  }
+
+  function handleNewTime(e){
+    setTime(e.target.value)
+    console.log(time)
+  }
 
   return (
     <div className='header'>
         <form onSubmit={handleSubmit}>
           <h4>Enter New Task</h4>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <h4>Time (in minutes)</h4>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <div className='button'></div>
+         <TextField id="outlined-basic" label="New Task" variant="outlined" onChange={handleNewActivity}/> 
+          <h4>Time (in minutes)</h4>
+          <TextField id="outlined-basic" label="Time" variant="outlined" onChange={handleNewTime}/> 
+          <div className='button'></div>
 
-            <Button variant="contained" type='submit'>Submit</Button>
+          <Button variant="contained" type='submit'>Submit</Button>
         </form>
     </div>
   );
