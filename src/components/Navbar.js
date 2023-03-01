3c316391a91b2c5dd6b6e10eabd0d3e3146a8ead
avinash singh <avinash8.as@gmail.com>
2023-03-01 18:01:32 -0500

    add navlinks to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Box from '@mui/material/Box';
+import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
   return (
@@ -16,10 +17,24 @@ export default function NavBar() {
       }}
     >
       <ButtonGroup variant="contained" aria-label="outlined button group" size='large'>
-        <Button>Home</Button>
-        <Button>Create New Task</Button>
-        <Button>Pending Tasks</Button>
-        <Button>Completed Tasks</Button>
+        <NavLink
+          to="/"
+          exact
+        >
+          <Button>Home</Button>
+        </NavLink>
+        <NavLink to="/newtask">
+          <Button>Create New Task</Button>
+        </NavLink>
+        <NavLink to="/pendingtasks">
+          <Button>Pending Tasks</Button>
+        </NavLink>
+        <NavLink to="/completedtasks">
+          <Button>Completed Tasks</Button>
+        </NavLink>
+        
+        
+        
       </ButtonGroup>
     </Box>
   );
